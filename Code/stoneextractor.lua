e73ee9238323d2498aa760d9f8cb02a828083ee0
Apethesis <apethesis@gmail.com>
2023-07-08 13:08:09 +0200

    did stuff

---
+++ Code/stoneextractor.lua
@@ -0,0 +1,11 @@
+local extractor = GetPartFromPort(1,"Extractor")
+local mat = {"Stone","Silicon","Sulfur","Flint","Oil","Ruby","Ice"}
+local cur = 1
+while true do
+    extractor:Configure({["MaterialToExtract"] = mat[cur]})
+    cur = cur + 1
+    if cur > #mat then
+        cur = 1
+    end
+    wait(0.9)
+end
