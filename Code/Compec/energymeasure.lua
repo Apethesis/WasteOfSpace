d13dc40d35a3536349360a9844d60a95da82dc88
Apethesis <apethesis@gmail.com>
2023-07-10 20:59:25 +0200

    energy difference measure thingy

---
+++ Code/Compec/energymeasure.lua
@@ -0,0 +1,33 @@
+local enerins = GetPartFromPort(1,"Instrument")
+local screen = GetPartFromPort(2,"Screen")
+local screennum = screen:CreateElement("TextLabel", {
+    ["Size"] = UDim2.new(1, 0, 1, 0),
+    ["TextScaled"] = true,
+    ["Font"] = Enum.Font.Code,
+    ["TextColor3"] = Color3.new(1,1,1),
+    ["Text"] = "0",
+    ["BorderSizePixel"] = 0
+})
+while true do
+    local enercur = enerins:GetReading(4)
+    wait(1.1)
+    local enernext = enerins:GetReading(4)
+    print(enercur, enernext)
+    local enerdiff = enernext - enercur
+    if enerdiff > 0 then
+        screennum:ChangeProperties({
+            ["TextColor3"] = Color3.new(0, 1, 0),
+            ["Text"] = "+"..enerdiff
+        })
+    elseif enerdiff == 0 then
+        screennum:ChangeProperties({
+            ["TextColor3"] = Color3.new(1,1,1),
+            ["Text"] = "0"
+        })
+    elseif enerdiff < 0 then
+        screennum:ChangeProperties({
+            ["TextColor3"] = Color3.new(1,0,0),
+            ["Text"] = enerdiff
+        })
+    end
+end
