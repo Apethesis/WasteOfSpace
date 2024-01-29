d32d32e9c96cefd02969a7796d46dafe97a3b2d7
Apethesis <apethesis@gmail.com>
2024-01-29 22:08:24 +0100

    ae

--- Code/Compec/reactorregulator.lua
+++ Code/Compec/reactorregulator.lua
@@ -1,24 +1,27 @@
 local reactor = GetPartFromPort(1, "Reactor")
 local dispenser = GetPartFromPort(2, "Dispenser")
 local instrument = GetPartFromPort(3, "Instrument")
-local polyactivate = GetPartFromPort(4, "Polysilicon")
-local polydeactivate = GetPartFromPort(5, "Polysilicon")
+
 while true do
     if instrument:GetReading(4) < 306667 then
-        if reactor:GetTemp() < 1000 then
-            polyactivate:Trigger()
-        elseif reactor:GetTemp() > 1000 then
-            polydeactivate:Trigger()
+        if reactor:GetTemp() > 1000 then
+            TriggerPort(4)
+
+        elseif reactor:GetTemp() < 1000 then
+            TriggerPort(5)
+
         elseif instrument:GetReading(4) > 306667 then
-            polydeactivate:Trigger()
+            TriggerPort(5)
         end
+
     else
-        polydeactivate:Trigger()
+        TriggerPort(4)
     end
+
     for _, amt in pairs(reactor:GetFuel()) do
         if amt == 0 then
             dispenser:Dispense()
         end
     end
-    task.wait(1)
+    wait(1)
 end
