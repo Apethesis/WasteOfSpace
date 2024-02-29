88b744a35d8a86cec557ef67b7dfe8a1087d83cf
Apethesis <apethesis@gmail.com>
2024-02-29 22:12:30 +0100

    S

--- Code/Compec/missiledesignator.lua
+++ Code/Compec/missiledesignator.lua
@@ -1,16 +1,31 @@
 local keyboard = GetPartFromPort(1,"Keyboard")
 local antenna = GetPartFromPort(1, "Port")
 local speaker = GetPartFromPort(1, "Speaker")
+local statusLight = GetPartFromPort(1, "Light"); statusLight:SetColor(Color3.fromRGB(255,0,0))
+local sNoneColor = Color3.fromRGB(255,0,0)
+local sIdleColor = Color3.fromRGB(0,0,255)
+local sLastColor
 local mislist = { existing = {} }
 antenna:Connect("Triggered", function(part)
     if not mislist.existing[part.GUID] then
         mislist.existing[part.GUID] = true
-        mislist[#mislist+1] = { GUID = part.GUID, detTriggerWire = GetPartFromPort(part,"Port"), anchor = GetPartFromPort(part,"Anchor"), gyro = GetPartFromPort(part,"Gyro"), detTrigger = GetPartFromPort(part,"TriggerSwitch"), fuelValve = GetPartFromPort(part,"Valve") }
+        mislist[#mislist+1] = { gyroTrigger = GetPartFromPort(part,"Port"), GUID = part.GUID, anchor = GetPartFromPort(part,"Anchor"), gyro = GetPartFromPort(part,"Gyro"), fuelValve = GetPartFromPort(part,"Valve") }
+        local triggers = GetPartsFromPort(part,"TriggerSwitch")
+        for k,v in pairs(triggers) do
+            if GetPartFromPort(v,"Transformer") then
+                mislist[#mislist].detTrigger = v
+            else
+                mislist[#mislist].primaryTrigger = v
+            end
+        end; triggers = nil
         local misid = #mislist
         speaker:Chat("Missile "..misid.." has been added to the table.")
-        mislist[#mislist].detTriggerWire:Connect("Triggered", function()
+        if sLastColor then local finColor = sLastColor:Lerp(sIdleColor,0.016); statusLight:SetColor(finColor); sLastColor = finColor else local finColor = sNoneColor:Lerp(sIdleColor,0.016); statusLight:SetColor(finColor); sLastColor = finColor; end 
+        mislist[#mislist].primaryTrigger:Connect("Triggered", function()
             speaker:Chat("Missile "..misid.." detonation wire has been triggered.")
             speaker:Chat("Removing from table...")
+            local finColor = sLastColor:Lerp(sNoneColor,0.016)
+            statusLight:SetColor(finColor); sLastColor = finColor
             mislist[misid] = nil
             mislist.existing[part.GUID] = nil
         end)
@@ -19,6 +34,7 @@ end)
 GetPort(2):Connect("Triggered", function()
     speaker:Chat("Global missile detonation signal was triggered.")
     speaker:Chat("Clearing table...")
+    statusLight:SetColor(sNoneColor); sLastColor = nil
     for k,v in pairs(mislist.existing) do
         mislist.existing[k] = nil
     end
@@ -44,19 +60,24 @@ keyboard:Connect("TextInputted", function(stri, plr)
             mislist[mid].detTrigger:Configure({ SwitchValue = true })
             speaker:Chat("Missile "..mid.." detonation wire has been triggered.")
             speaker:Chat("Removing from table...")
+            local finColor = sLastColor:Lerp(sNoneColor,0.016)
+            statusLight:SetColor(finColor); sLastColor = finColor
             mislist.existing[mislist[mid].GUID] = nil
             mislist[mid] = nil
         elseif cmdres == "launch" then
             speaker:Chat("Attempting to launch missile number "..mid)
             mislist[mid].fuelValve:Configure({ SwitchValue = true })
             mislist[mid].anchor:Configure({ Anchored = false })
+            task.wait(5)
+            mislist[mid].primaryTrigger:Configure({ SwitchValue = true })
+            GetPartFromPort(mislist[mid].gyroTrigger, "DelayWire"):Trigger()
         elseif cmdres == "anchor" then
-            mislist[mid].Anchor:Configure({ Anchored = true })
+            mislist[mid].anchor:Configure({ Anchored = true })
         elseif cmdres == "unanchor" then
-            mislist[mid].Anchor:Configure({ Anchored = false })
+            mislist[mid].anchor:Configure({ Anchored = false })
         elseif cmdres == "drop" then
             mislist[mid].fuelValve:Configure({ SwitchValue = false })
-            mislist[mid].Anchor:Configure({ Anchored = false })
+            mislist[mid].anchor:Configure({ Anchored = false })
         end
     else
         speaker:Chat("Invalid DESSOL ID.")
--- Code/Compec/temperaturereg.lua
+++ Code/Compec/temperaturereg.lua
@@ -1,41 +1,57 @@
 local coolers = GetPartsFromPort(1, "Switch")
-local heaters = GetPartsFromPort(1, "Switch")
+local heaters = GetPartsFromPort(2, "Switch")
 local instrument = GetPartFromPort(4, "Instrument")
+local diff = 0
+for k,v in pairs(coolers) do
+    v:Configure({ SwitchValue = false })
+end
+for k,v in pairs(heaters) do
+    v:Configure({ SwitchValue = false })
+end
 local function adjustswitch(am)
     if am == 0 then
-        for k,v in ipairs(coolers) do
+        diff = 0
+        for k,v in pairs(coolers) do
             v:Configure({ SwitchValue = false })
         end
-        for k,v in ipairs(heaters) do
+        for k,v in pairs(heaters) do
             v:Configure({ SwitchValue = false })
         end
     elseif am > 0 then
-        for k,v in ipairs(coolers) do
+        diff = 0
+        for k,v in pairs(coolers) do
             v:Configure({ SwitchValue = false })
         end
         for i=1,am do
+            diff = diff - 10
             heaters[i]:Configure({ SwitchValue = true })
         end
     elseif am < 0 then
-        for k,v in ipairs(heaters) do
+        diff = 0
+        am = math.abs(am)
+        for k,v in pairs(heaters) do
             v:Configure({ SwitchValue = false })
         end
         for i=1,am do
+            diff = diff + 20
             coolers[i]:Configure({ SwitchValue = true })
         end
     end
     return am
 end
 while true do
-    if instrument:GetReading(7) < 0 then
-        print(adjustswitch(math.round(instrument:GetReading(7)/20)))
-    elseif instrument:GetReading(7) > 70 then
-        print(adjustswitch(-(math.round(instrument:GetReading(7)/10))))
-    else
+    local curread = instrument:GetReading(7)
+    print(curread)
+    print(curread+diff)
+    if instrument:GetReading(7) < (0+diff) then 
+        print(adjustswitch(math.round(math.abs(instrument:GetReading(7))/10)))
+    elseif instrument:GetReading(7) > (70+diff) then
+        print(adjustswitch(-(math.round(((instrument:GetReading(7)-70)+diff)/20))))
+    elseif diff == 0 then
         adjustswitch(0)
     end
     if instrument:GetReading(0) > 750 then
         TriggerPort(3)
     end
-    task.wait(1)
+    task.wait(2)
 end
