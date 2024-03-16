2bddebea94edf1e6943c419dd5311492bbeab376
Apethesis <apethesis@gmail.com>
2024-03-17 00:44:15 +0100

    done and to be done

--- Code/Compec/missiledesignator.lua
+++ Code/Compec/missiledesignator.lua
@@ -9,22 +9,35 @@ local mislist = { existing = {} }
 antenna:Connect("Triggered", function(part)
     if not mislist.existing[part.GUID] then
         mislist.existing[part.GUID] = true
-        mislist[#mislist+1] = { gyroTrigger = GetPartFromPort(part,"Port"), GUID = part.GUID, anchor = GetPartFromPort(part,"Anchor"), gyro = GetPartFromPort(part,"Gyro"), fuelValve = GetPartFromPort(part,"Valve") }
+        mislist[#mislist+1] = {
+            GUID = part.GUID, 
+            anchor = GetPartFromPort(part,"Anchor"), 
+            gyro = GetPartFromPort(part,"Gyro"), 
+            fuelValve = GetPartFromPort(part,"Valve")
+        }
         local triggers = GetPartsFromPort(part,"TriggerSwitch")
+        local ports = GetPartsFromPort(part,"Port")
         for k,v in pairs(triggers) do
             if GetPartFromPort(v,"Transformer") then
                 mislist[#mislist].detTrigger = v
+            elseif GetPartFromPort(v,"DelayWire") then
+                mislist[#mislist].gyroTrigger = v
             else
                 mislist[#mislist].primaryTrigger = v
             end
         end; triggers = nil
+        for k,v in pairs(ports) do
+            if GetPartFromPort(v,"Transformer") then
+                mislist[#mislist].gyroTrigPort = v
+            end
+        end; ports = nil
         local misid = #mislist
         speaker:Chat("Missile "..misid.." has been added to the table.")
-        if sLastColor then local finColor = sLastColor:Lerp(sIdleColor,0.016); statusLight:SetColor(finColor); sLastColor = finColor else local finColor = sNoneColor:Lerp(sIdleColor,0.016); statusLight:SetColor(finColor); sLastColor = finColor; end 
+        if sLastColor then local finColor = sLastColor:Lerp(sIdleColor,0.050); statusLight:SetColor(finColor); sLastColor = finColor else local finColor = sNoneColor:Lerp(sIdleColor,0.050); statusLight:SetColor(finColor); sLastColor = finColor; end 
         mislist[#mislist].primaryTrigger:Connect("Triggered", function()
             speaker:Chat("Missile "..misid.." detonation wire has been triggered.")
             speaker:Chat("Removing from table...")
-            local finColor = sLastColor:Lerp(sNoneColor,0.016)
+            local finColor = sLastColor:Lerp(sNoneColor,0.050)
             statusLight:SetColor(finColor); sLastColor = finColor
             mislist[misid] = nil
             mislist.existing[part.GUID] = nil
@@ -63,7 +76,7 @@ keyboard:Connect("TextInputted", function(stri, plr)
                     mislist[mid].detTrigger:Configure({ SwitchValue = true })
                     speaker:Chat("Missile "..mid.." detonation wire has been triggered.")
                     speaker:Chat("Removing from table...")
-                    local finColor = sLastColor:Lerp(sNoneColor,0.016)
+                    local finColor = sLastColor:Lerp(sNoneColor,0.050)
                     statusLight:SetColor(finColor); sLastColor = finColor
                     mislist.existing[mislist[mid].GUID] = nil
                     mislist[mid] = nil
@@ -71,9 +84,13 @@ keyboard:Connect("TextInputted", function(stri, plr)
                     speaker:Chat("Attempting to launch missile number "..mid)
                     mislist[mid].fuelValve:Configure({ SwitchValue = true })
                     mislist[mid].anchor:Configure({ Anchored = false })
-                    task.wait(5)
-                    mislist[mid].primaryTrigger:Configure({ SwitchValue = true })
-                    GetPartFromPort(mislist[mid].gyroTrigger, "DelayWire"):Trigger()
+                    mislist[mid].gyroTrigger:Configure({ SwitchValue = true })
+                    local trigThing
+                    trigThing = mislist[mid].gyroTrigPort:Connect("Triggered", function()
+                        task.wait(0.1)
+                        mislist[mid].gyroTrigger:Configure({ SwitchValue = false })
+                        trigThing:Unbind()
+                    end)
                 elseif cmdres == "anchor" then
                     mislist[mid].anchor:Configure({ Anchored = true })
                 elseif cmdres == "unanchor" then
@@ -85,6 +102,12 @@ keyboard:Connect("TextInputted", function(stri, plr)
                     mislist[mid].fuelValve:Configure({ SwitchValue = true })
                 elseif cmdres == "close" then
                     mislist[mid].fuelValve:Configure({ SwitchValue = false })
+                elseif cmdres == "remove" then
+                    speaker:Chat("Removing missile "..mid.." from table...")
+                    local finColor = sLastColor:Lerp(sNoneColor,0.050)
+                    statusLight:SetColor(finColor); sLastColor = finColor
+                    mislist.existing[mislist[mid].GUID] = nil
+                    mislist[mid] = nil
                 end
             else
                 speaker:Chat("Invalid DESSOL ID.")
@@ -101,7 +124,7 @@ keyboard:Connect("TextInputted", function(stri, plr)
                 mislist[mid].detTrigger:Configure({ SwitchValue = true })
                 speaker:Chat("Missile "..mid.." detonation wire has been triggered.")
                 speaker:Chat("Removing from table...")
-                local finColor = sLastColor:Lerp(sNoneColor,0.016)
+                local finColor = sLastColor:Lerp(sNoneColor,0.050)
                 statusLight:SetColor(finColor); sLastColor = finColor
                 mislist.existing[mislist[mid].GUID] = nil
                 mislist[mid] = nil
@@ -109,9 +132,13 @@ keyboard:Connect("TextInputted", function(stri, plr)
                 speaker:Chat("Attempting to launch missile number "..mid)
                 mislist[mid].fuelValve:Configure({ SwitchValue = true })
                 mislist[mid].anchor:Configure({ Anchored = false })
-                task.wait(5)
-                mislist[mid].primaryTrigger:Configure({ SwitchValue = true })
-                GetPartFromPort(mislist[mid].gyroTrigger, "DelayWire"):Trigger()
+                mislist[mid].gyroTrigger:Configure({ SwitchValue = true })
+                local trigThing
+                trigThing = mislist[mid].gyroTrigPort:Connect("Triggered", function()
+                    task.wait(0.1)
+                    mislist[mid].gyroTrigger:Configure({ SwitchValue = false })
+                    trigThing:Unbind()
+                end)
             elseif cmdres == "anchor" then
                 mislist[mid].anchor:Configure({ Anchored = true })
             elseif cmdres == "unanchor" then
@@ -123,6 +150,12 @@ keyboard:Connect("TextInputted", function(stri, plr)
                 mislist[mid].fuelValve:Configure({ SwitchValue = true })
             elseif cmdres == "close" then
                 mislist[mid].fuelValve:Configure({ SwitchValue = false })
+            elseif cmdres == "remove" then
+                speaker:Chat("Removing missile "..mid.." from table...")
+                local finColor = sLastColor:Lerp(sNoneColor,0.050)
+                statusLight:SetColor(finColor); sLastColor = finColor
+                mislist.existing[mislist[mid].GUID] = nil
+                mislist[mid] = nil
             end
         else
             speaker:Chat("Invalid DESSOL ID.")
